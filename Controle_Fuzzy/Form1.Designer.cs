0f8b290a60dc720548e3d7a823a37e47e4ca6059
neudinho11 <neudinho66@gmail.com>
2019-04-15 18:09:16 -0300

    Criação da tela.

--- Controle_Fuzzy/Form1.Designer.cs
+++ Controle_Fuzzy/Form1.Designer.cs
@@ -28,33 +28,187 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btConectar = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbBomba = new System.Windows.Forms.Label();
+            this.lbNivel = new System.Windows.Forms.Label();
+            this.rbAutomatico = new System.Windows.Forms.RadioButton();
+            this.rbManual = new System.Windows.Forms.RadioButton();
+            this.btIniciar = new System.Windows.Forms.Button();
+            this.btSalvar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // btConectar
             // 
-            this.button1.Location = new System.Drawing.Point(369, 163);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btConectar.Location = new System.Drawing.Point(830, 119);
+            this.btConectar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btConectar.Name = "btConectar";
+            this.btConectar.Size = new System.Drawing.Size(100, 28);
+            this.btConectar.TabIndex = 0;
+            this.btConectar.Text = "Conectar";
+            this.btConectar.UseVisualStyleBackColor = true;
+            this.btConectar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(626, 123);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(141, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(528, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Portas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(528, 198);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nivel";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(525, 275);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Bomba";
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(626, 275);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(266, 21);
+            this.hScrollBar1.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbManual);
+            this.groupBox1.Controls.Add(this.rbAutomatico);
+            this.groupBox1.Location = new System.Drawing.Point(531, 365);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(280, 150);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Funcionamento da bomba";
+            // 
+            // lbBomba
+            // 
+            this.lbBomba.AutoSize = true;
+            this.lbBomba.Location = new System.Drawing.Point(940, 279);
+            this.lbBomba.Name = "lbBomba";
+            this.lbBomba.Size = new System.Drawing.Size(36, 17);
+            this.lbBomba.TabIndex = 7;
+            this.lbBomba.Text = "30%";
+            // 
+            // lbNivel
+            // 
+            this.lbNivel.AutoSize = true;
+            this.lbNivel.Location = new System.Drawing.Point(623, 198);
+            this.lbNivel.Name = "lbNivel";
+            this.lbNivel.Size = new System.Drawing.Size(42, 17);
+            this.lbNivel.TabIndex = 8;
+            this.lbNivel.Text = "30cm";
+            // 
+            // rbAutomatico
+            // 
+            this.rbAutomatico.AutoSize = true;
+            this.rbAutomatico.Location = new System.Drawing.Point(24, 41);
+            this.rbAutomatico.Name = "rbAutomatico";
+            this.rbAutomatico.Size = new System.Drawing.Size(99, 21);
+            this.rbAutomatico.TabIndex = 0;
+            this.rbAutomatico.TabStop = true;
+            this.rbAutomatico.Text = "Automatico";
+            this.rbAutomatico.UseVisualStyleBackColor = true;
+            // 
+            // rbManual
+            // 
+            this.rbManual.AutoSize = true;
+            this.rbManual.Location = new System.Drawing.Point(24, 92);
+            this.rbManual.Name = "rbManual";
+            this.rbManual.Size = new System.Drawing.Size(75, 21);
+            this.rbManual.TabIndex = 1;
+            this.rbManual.TabStop = true;
+            this.rbManual.Text = "Manual";
+            this.rbManual.UseVisualStyleBackColor = true;
+            // 
+            // btIniciar
+            // 
+            this.btIniciar.Location = new System.Drawing.Point(66, 433);
+            this.btIniciar.Margin = new System.Windows.Forms.Padding(4);
+            this.btIniciar.Name = "btIniciar";
+            this.btIniciar.Size = new System.Drawing.Size(100, 28);
+            this.btIniciar.TabIndex = 9;
+            this.btIniciar.Text = "Iniciar";
+            this.btIniciar.UseVisualStyleBackColor = true;
+            // 
+            // btSalvar
+            // 
+            this.btSalvar.Location = new System.Drawing.Point(285, 433);
+            this.btSalvar.Margin = new System.Windows.Forms.Padding(4);
+            this.btSalvar.Name = "btSalvar";
+            this.btSalvar.Size = new System.Drawing.Size(100, 28);
+            this.btSalvar.TabIndex = 10;
+            this.btSalvar.Text = "Salvar";
+            this.btSalvar.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.btSalvar);
+            this.Controls.Add(this.btIniciar);
+            this.Controls.Add(this.lbNivel);
+            this.Controls.Add(this.lbBomba);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btConectar);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Controle Fuzzy";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btConectar;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbManual;
+        private System.Windows.Forms.RadioButton rbAutomatico;
+        private System.Windows.Forms.Label lbBomba;
+        private System.Windows.Forms.Label lbNivel;
+        private System.Windows.Forms.Button btIniciar;
+        private System.Windows.Forms.Button btSalvar;
     }
 }
 
--- Controle_Fuzzy/Form1.cs
+++ Controle_Fuzzy/Form1.cs
@@ -16,5 +16,10 @@ namespace Controle_Fuzzy
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
