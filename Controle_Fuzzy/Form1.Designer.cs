b262c3e2ee654d3a5a85460b05e9ebf65c464cd2
apla77 <verocidade77@gmail.com>
2019-04-15 18:46:07 -0300

    Conexão serial com Arduino

--- Controle_Fuzzy/Form1.Designer.cs
+++ Controle_Fuzzy/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btConectar = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -35,144 +36,159 @@
             this.label3 = new System.Windows.Forms.Label();
             this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbManual = new System.Windows.Forms.RadioButton();
+            this.rbAutomatico = new System.Windows.Forms.RadioButton();
             this.lbBomba = new System.Windows.Forms.Label();
             this.lbNivel = new System.Windows.Forms.Label();
-            this.rbAutomatico = new System.Windows.Forms.RadioButton();
-            this.rbManual = new System.Windows.Forms.RadioButton();
             this.btIniciar = new System.Windows.Forms.Button();
             this.btSalvar = new System.Windows.Forms.Button();
+            this.timerCOM = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btConectar
             // 
-            this.btConectar.Location = new System.Drawing.Point(830, 119);
-            this.btConectar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btConectar.Location = new System.Drawing.Point(622, 97);
             this.btConectar.Name = "btConectar";
-            this.btConectar.Size = new System.Drawing.Size(100, 28);
+            this.btConectar.Size = new System.Drawing.Size(75, 23);
             this.btConectar.TabIndex = 0;
             this.btConectar.Text = "Conectar";
             this.btConectar.UseVisualStyleBackColor = true;
-            this.btConectar.Click += new System.EventHandler(this.button1_Click);
+            this.btConectar.Click += new System.EventHandler(this.btConectar_Click);
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(626, 123);
+            this.comboBox1.Location = new System.Drawing.Point(470, 100);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(141, 24);
+            this.comboBox1.Size = new System.Drawing.Size(107, 21);
             this.comboBox1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(528, 130);
+            this.label1.Location = new System.Drawing.Point(396, 106);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.Size = new System.Drawing.Size(37, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Portas";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(528, 198);
+            this.label2.Location = new System.Drawing.Point(396, 161);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(39, 17);
+            this.label2.Size = new System.Drawing.Size(31, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Nivel";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(525, 275);
+            this.label3.Location = new System.Drawing.Point(394, 223);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(52, 17);
+            this.label3.Size = new System.Drawing.Size(40, 13);
             this.label3.TabIndex = 4;
             this.label3.Text = "Bomba";
             // 
             // hScrollBar1
             // 
-            this.hScrollBar1.Location = new System.Drawing.Point(626, 275);
+            this.hScrollBar1.Location = new System.Drawing.Point(470, 223);
             this.hScrollBar1.Name = "hScrollBar1";
-            this.hScrollBar1.Size = new System.Drawing.Size(266, 21);
+            this.hScrollBar1.Size = new System.Drawing.Size(200, 21);
             this.hScrollBar1.TabIndex = 5;
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.rbManual);
             this.groupBox1.Controls.Add(this.rbAutomatico);
-            this.groupBox1.Location = new System.Drawing.Point(531, 365);
+            this.groupBox1.Location = new System.Drawing.Point(398, 297);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(280, 150);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(210, 122);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Funcionamento da bomba";
             // 
+            // rbManual
+            // 
+            this.rbManual.AutoSize = true;
+            this.rbManual.Location = new System.Drawing.Point(18, 75);
+            this.rbManual.Margin = new System.Windows.Forms.Padding(2);
+            this.rbManual.Name = "rbManual";
+            this.rbManual.Size = new System.Drawing.Size(60, 17);
+            this.rbManual.TabIndex = 1;
+            this.rbManual.TabStop = true;
+            this.rbManual.Text = "Manual";
+            this.rbManual.UseVisualStyleBackColor = true;
+            // 
+            // rbAutomatico
+            // 
+            this.rbAutomatico.AutoSize = true;
+            this.rbAutomatico.Location = new System.Drawing.Point(18, 33);
+            this.rbAutomatico.Margin = new System.Windows.Forms.Padding(2);
+            this.rbAutomatico.Name = "rbAutomatico";
+            this.rbAutomatico.Size = new System.Drawing.Size(78, 17);
+            this.rbAutomatico.TabIndex = 0;
+            this.rbAutomatico.TabStop = true;
+            this.rbAutomatico.Text = "Automatico";
+            this.rbAutomatico.UseVisualStyleBackColor = true;
+            // 
             // lbBomba
             // 
             this.lbBomba.AutoSize = true;
-            this.lbBomba.Location = new System.Drawing.Point(940, 279);
+            this.lbBomba.Location = new System.Drawing.Point(705, 227);
+            this.lbBomba.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbBomba.Name = "lbBomba";
-            this.lbBomba.Size = new System.Drawing.Size(36, 17);
+            this.lbBomba.Size = new System.Drawing.Size(27, 13);
             this.lbBomba.TabIndex = 7;
             this.lbBomba.Text = "30%";
             // 
             // lbNivel
             // 
             this.lbNivel.AutoSize = true;
-            this.lbNivel.Location = new System.Drawing.Point(623, 198);
+            this.lbNivel.Location = new System.Drawing.Point(467, 161);
+            this.lbNivel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbNivel.Name = "lbNivel";
-            this.lbNivel.Size = new System.Drawing.Size(42, 17);
+            this.lbNivel.Size = new System.Drawing.Size(33, 13);
             this.lbNivel.TabIndex = 8;
             this.lbNivel.Text = "30cm";
             // 
-            // rbAutomatico
-            // 
-            this.rbAutomatico.AutoSize = true;
-            this.rbAutomatico.Location = new System.Drawing.Point(24, 41);
-            this.rbAutomatico.Name = "rbAutomatico";
-            this.rbAutomatico.Size = new System.Drawing.Size(99, 21);
-            this.rbAutomatico.TabIndex = 0;
-            this.rbAutomatico.TabStop = true;
-            this.rbAutomatico.Text = "Automatico";
-            this.rbAutomatico.UseVisualStyleBackColor = true;
-            // 
-            // rbManual
-            // 
-            this.rbManual.AutoSize = true;
-            this.rbManual.Location = new System.Drawing.Point(24, 92);
-            this.rbManual.Name = "rbManual";
-            this.rbManual.Size = new System.Drawing.Size(75, 21);
-            this.rbManual.TabIndex = 1;
-            this.rbManual.TabStop = true;
-            this.rbManual.Text = "Manual";
-            this.rbManual.UseVisualStyleBackColor = true;
-            // 
             // btIniciar
             // 
-            this.btIniciar.Location = new System.Drawing.Point(66, 433);
-            this.btIniciar.Margin = new System.Windows.Forms.Padding(4);
+            this.btIniciar.Location = new System.Drawing.Point(50, 352);
             this.btIniciar.Name = "btIniciar";
-            this.btIniciar.Size = new System.Drawing.Size(100, 28);
+            this.btIniciar.Size = new System.Drawing.Size(75, 23);
             this.btIniciar.TabIndex = 9;
             this.btIniciar.Text = "Iniciar";
             this.btIniciar.UseVisualStyleBackColor = true;
+            this.btIniciar.Click += new System.EventHandler(this.btIniciar_Click);
             // 
             // btSalvar
             // 
-            this.btSalvar.Location = new System.Drawing.Point(285, 433);
-            this.btSalvar.Margin = new System.Windows.Forms.Padding(4);
+            this.btSalvar.Location = new System.Drawing.Point(214, 352);
             this.btSalvar.Name = "btSalvar";
-            this.btSalvar.Size = new System.Drawing.Size(100, 28);
+            this.btSalvar.Size = new System.Drawing.Size(75, 23);
             this.btSalvar.TabIndex = 10;
             this.btSalvar.Text = "Salvar";
             this.btSalvar.UseVisualStyleBackColor = true;
             // 
+            // timerCOM
+            // 
+            this.timerCOM.Interval = 1000;
+            this.timerCOM.Tick += new System.EventHandler(this.timerCOM_Tick);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(825, 495);
             this.Controls.Add(this.btSalvar);
             this.Controls.Add(this.btIniciar);
             this.Controls.Add(this.lbNivel);
@@ -184,7 +200,6 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.btConectar);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Controle Fuzzy";
             this.groupBox1.ResumeLayout(false);
@@ -209,6 +224,8 @@
         private System.Windows.Forms.Label lbNivel;
         private System.Windows.Forms.Button btIniciar;
         private System.Windows.Forms.Button btSalvar;
+        private System.Windows.Forms.Timer timerCOM;
+        private System.IO.Ports.SerialPort serialPort1;
     }
 }
 
--- Controle_Fuzzy/Form1.cs
+++ Controle_Fuzzy/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports; // para ter acesso as portas.
 
 namespace Controle_Fuzzy
 {
@@ -15,9 +16,102 @@ namespace Controle_Fuzzy
         public Form1()
         {
             InitializeComponent();
+            timerCOM.Enabled = true;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void atualizaListaCOMs()
+        {
+            int i;
+            bool quantDiferente; //flag para sinalizar que a quantidade de portas mudou
+
+            i = 0;
+            quantDiferente = false;
+
+            //se a quantidade de portas mudou
+            if (comboBox1.Items.Count == SerialPort.GetPortNames().Length)
+            {
+                foreach (string s in SerialPort.GetPortNames())
+                {
+                    if (comboBox1.Items[i++].Equals(s) == false)
+                    {
+                        quantDiferente = true;
+                    }
+                }
+            }
+            else
+            {
+                quantDiferente = true;
+            }
+
+            //Se não foi detectado diferença
+            if (quantDiferente == false)
+            {
+                return;                     //retorna
+            }
+
+            //limpa comboBox
+            comboBox1.Items.Clear();
+
+            //adiciona todas as COM diponíveis na lista
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                comboBox1.Items.Add(s);
+            }
+            //seleciona a primeira posição da lista
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void btConectar_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen == false)
+            {
+                try
+                {
+                    serialPort1.PortName = comboBox1.Items[comboBox1.SelectedIndex].ToString();
+                    serialPort1.Open();
+
+                }
+                catch
+                {
+                    return;
+
+                }
+                if (serialPort1.IsOpen)
+                {
+                    btConectar.Text = "Desconectar";
+                    comboBox1.Enabled = false;
+
+                }
+            }
+            else
+            {
+
+                try
+                {
+                    serialPort1.Close();
+                    comboBox1.Enabled = true;
+                    btConectar.Text = "Conectar";
+                }
+                catch
+                {
+                    return;
+                }
+
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (serialPort1.IsOpen == true)  // se porta aberta
+                serialPort1.Close();         //fecha a porta
+        }
+
+        private void timerCOM_Tick(object sender, EventArgs e)
+        {
+            atualizaListaCOMs();
+        }
+
+        private void btIniciar_Click(object sender, EventArgs e)
         {
 
         }
